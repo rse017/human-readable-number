2920e3b272b09a073b1e696a5ab0d5555fbcc603
Sergei R <rse017@gmail.com>
2022-08-11 01:54:04 +0300

    feat:solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
-  
-}
+
+const a = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+const b = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+        
+        if (number < 20) return a[number];
+        if (number < 100) {
+            if(number%10 > 0)
+            return b[Math.floor(number/10)-2] + ' ' + a[number%10];
+            else
+            return b[Math.floor(number/10)-2];
+        }
+        if (number < 1000) {
+            if(number%100 > 0)
+            return a[Math.floor(number/100)] +' '+'hundred' +  " " + toReadable(number%100);
+            else
+            return a[Math.floor(number/100)] +' '+'hundred' ;
+        }
+    } 
+
+
+
